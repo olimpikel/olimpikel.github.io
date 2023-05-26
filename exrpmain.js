c26ffbb9098e87e62c1b9ab91a30a8daa9093637
olimpikel <131589079+olimpikel@users.noreply.github.com>
2023-05-26 16:59:36 +0200

    Update exrpmain.js

--- exrpmain.js
+++ exrpmain.js
@@ -946,5 +946,16 @@ async function pourcentagecollateralisationutilisateur() {
     }
 }
 
+async function getAverageCollateralization() {
+    try {
+        const web3 = new Web3(window.ethereum);
+        const contract = new web3.eth.Contract(abi, contractAddress);
+        const accounts = await web3.eth.getAccounts();
+        const result3 = await contract.methods.getAverageCollateralization().call({ from: accounts[0] });
+document.getElementById("result2").innerHTML = result3;
 
+    } catch (error) {
+        console.error("An error occurred: ", error);
+    }
+}
 
